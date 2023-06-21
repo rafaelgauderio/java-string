42265ca206231bb5ceef1e4b4f6fcb523df9cdd4
Rafael de Luca <deluca1712@gmail.com>
2023-06-21 12:03:01 -0300

    Using split function with a Class

--- application/ProgramSplitJoin.java
+++ application/ProgramSplitJoin.java
@@ -1,8 +1,14 @@
 package application;
 
+import java.util.Locale;
+
+import entities.Product;
+
 public class ProgramSplitJoin {
 
 	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
 		
 		String original = new String("Rafael de Luca is a java developer");
 		
@@ -13,6 +19,8 @@ public class ProgramSplitJoin {
 			System.out.println(array[i]);
 		}
 		
+		////////////////////////////////
+		
 		String livro1 = "A Elite do atraso";
 		String livro2 = "A ralé brasileira";
 		String livro3 = "A classe média no espelho";
@@ -20,6 +28,20 @@ public class ProgramSplitJoin {
 		System.out.println("\nPrinting join string by coma");
 		String joinLivros = String.join(", ",livro1,livro2, livro3);
 		System.out.println(joinLivros);
+		
+		/////////////////////////
+		String product = ("1920,Calça jeans preta tamanho 42,105.90");
+		String [] productInformation = product.split(",");
+		
+		System.out.println("\nPrinting product information");
+		for(String nickname : productInformation) {
+			System.out.println(nickname);
+		}
+		
+		Product produto = new Product(Long.parseLong(productInformation[0]),productInformation[1],Double.parseDouble(productInformation[2]));
+		System.out.println(produto);
+		
+		
 		
 		
 		
