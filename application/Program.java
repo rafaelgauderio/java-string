f4b18b0722315e6351ae0c05efdb75d82b7d17ff
Rafael de Luca <deluca1712@gmail.com>
2023-06-21 10:09:08 -0300

    Create Project

---
+++ application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String original = "   Rafael de Luca is a java developer.               ";
+		
+		System.out.println("Original sentence: *" + original + "*"  );
+		
+
+	}
+
+}
---
+++ module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rafael
+ *
+ */
+module string {
+}
